4d34d54e89dc618cce2127a31df7a8f51f4c35a3
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2026-05-24 23:28:14 +0500

    Replace 'std::cout' statements with the log functions. Remove the related TODO comments. Rewrite the TODO comment about specifying that the printing of the 'ClassFile' variable is only for testing purposes.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "parser/parser.h"
 
+#include "base.h"
 #include "parser/class_file.h"
 
 #include <iostream>
@@ -9,21 +10,18 @@
 int main()
 {
     std::ifstream class_file_src{"Main.class", std::ios::binary };
+
     if (class_file_src.fail())
-    {
-        // TODO: Replace with a log function/method
-        std::cout << "Failed to open the class file." << std::endl;
-        return 1;
-    }
-    // TODO: Replace with a log function/method
-    std::cout << "Successfully opened the class file.\n";
+        log_fatal("Failed to open the class file.");
+
+    log_debug("Successfully opened the class file.");
 
     ClassFileReader cf_reader{ class_file_src };
     ClassFileParser cf_parser{ cf_reader };
 
     ClassFile cf{ cf_parser.parse() };
 
-    // For testing only
+    // TODO: SHOULD BE REMOVED LATER: For testing only!!!
     std::cout << cf << std::endl;
 
     return 0;
